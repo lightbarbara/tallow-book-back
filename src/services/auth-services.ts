d511683d57794b0fb248d0fc30946cb63c2507f4
Barbara Luz <barbaraluz@eq.ufrj.br>
2023-04-14 14:16:46 -0300

    feat: finish auth routes

--- src/services/auth-services.ts
+++ src/services/auth-services.ts
@@ -1,7 +1,9 @@
+import { users } from "@prisma/client"
 import authRepositories from "../repositories/auth-repositories"
 import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
 
-async function signUp(name: string, email: string, avatar: string, password: string) {
+async function signUp(name: string, email: string, avatar: string, password: string): Promise<users> {
     const emailRegistered = await authRepositories.findUserByEmail(email)
 
     if (emailRegistered) {
@@ -17,8 +19,24 @@ async function signUp(name: string, email: string, avatar: string, password: str
     return user
 }
 
-async function signIn(email: string, password: string) {
+async function signIn(email: string, password: string): Promise<{ user: users, token: string }> {
     const user = await authRepositories.findUserByEmail(email)
+
+    if (!user) {
+        throw { name: 'invalidUser' }
+    }
+
+    const isPasswordRight = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordRight) {
+        throw { name: 'invalidUser' }
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET)
+
+    delete user.password
+
+    return { user, token }
 }
 
 const authServices = {
