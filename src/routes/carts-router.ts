5af79688aedb1761a4232c39be04f96dd342215d
Barbara Luz <barbaraluz@eq.ufrj.br>
2023-04-16 20:05:15 -0300

    refact: delete comments

--- src/routes/carts-router.ts
+++ src/routes/carts-router.ts
@@ -6,7 +6,7 @@ const cartsRouter = Router()
 
 cartsRouter.post('/cart', validateAuth, postCart)
 cartsRouter.get('/cart', validateAuth, getCart)
-cartsRouter.delete('/cart/book/:id', validateAuth, deleteBookInCart) // criar
+cartsRouter.delete('/cart/book/:id', validateAuth, deleteBookInCart)
 cartsRouter.post('/finish-cart', validateAuth, finishCart) // colocar pagamento
 
 export default cartsRouter
